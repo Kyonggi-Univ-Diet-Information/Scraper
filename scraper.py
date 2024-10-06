9bf5f72c79c30dbb2bd660763fcb46d093b27c25
boroboro01 <duke7201@naver.com>
2024-10-06 16:30:28 +0900

    feat: create formatted url and get response by html

---
+++ scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+
+now = datetime.datetime.now()
+
+year = now.year
+month = now.strftime("%m")
+day = now.strftime("%d")
+
+url = f'https://dorm.kyonggi.ac.kr:446/Khostel/mall_main.php?viewform=B0001_foodboard_list&gyear={year}&gmonth={month}&gday={day}'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup)
