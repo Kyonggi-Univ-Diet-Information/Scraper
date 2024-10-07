b38f2d1106e9cfe28b2d4cc55a0da05a1cf34bfd
boroboro01 <duke7201@naver.com>
2024-10-08 00:20:26 +0900

    feat: add loggin login for spider

--- scraper/main.py
+++ scraper/main.py
@@ -1,7 +1,22 @@
+import logging
+import datetime
 from scrapy.crawler import CrawlerProcess
 from scraper.spiders.dorm import DormSpider
+from scrapy.utils.project import get_project_settings
+
 
 def run_spider():
+    settings = get_project_settings()
+
+    current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_filename = f'logs/scrapy_log_{current_time}.txt'
+
+    logging.basicConfig(
+        filename=log_filename,
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        level=logging.DEBUG
+    )
+
     process = CrawlerProcess()
     process.crawl(DormSpider)
     process.start()
