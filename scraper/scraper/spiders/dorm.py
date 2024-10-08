3403aea6a10c9e2ae0496b9ab58c565cbfda2226
boroboro01 <duke7201@naver.com>
2024-10-08 21:06:39 +0900

    feat: add string to blank data

--- scraper/scraper/spiders/dorm.py
+++ scraper/scraper/spiders/dorm.py
@@ -1,31 +1,30 @@
 import scrapy
 from datetime import datetime
+from scraper.items import ScraperItem
 
 class DormSpider(scrapy.Spider):
     name = "dorm"
     allowed_domains = ["dorm.kyonggi.ac.kr"]
 
     def start_requests(self):
-            # 현재 날짜를 가져옵니다.
-            today = datetime.now()
-            year = today.strftime("%Y")
-            month = today.strftime("%m")
-            day = today.strftime("%d")
+        today = datetime.now()
+        year = today.strftime("%Y")
+        month = today.strftime("%m")
+        day = today.strftime("%d")
 
-            # 동적으로 URL을 생성합니다.
-            url = f"https://dorm.kyonggi.ac.kr:446/Khostel/mall_main.php?viewform=B0001_foodboard_list&gyear={year}&gmonth={month}&gday={day}"
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = f"https://dorm.kyonggi.ac.kr:446/Khostel/mall_main.php?viewform=B0001_foodboard_list&gyear={year}&gmonth={month}&gday={day}"
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         decoded_body = response.body.decode('euc-kr')
 
         for row in response.css("table.boxstyle02 tbody tr"):
-
             date = row.css("th a::text").get() or row.css("th::text").get(default="").strip()
             meals = row.css("td")
-            breakfast = "No data"
-            lunch = "No data"
-            dinner = "No data"
+            
+            breakfast = ""
+            lunch = ""
+            dinner = ""
 
             if len(meals) > 0:
                 breakfast = "&".join([item.strip() for item in meals[0].css("::text").getall() if item.strip()])
@@ -36,8 +35,7 @@ class DormSpider(scrapy.Spider):
 
             yield {
                 "date": date,
-                "breakfast": breakfast,
-                "lunch": lunch,
-                "dinner": dinner
+                "breakfast": breakfast if breakfast else "미운영",
+                "lunch": lunch if lunch else "미운영",
+                "dinner": dinner if dinner else "미운영",
             }
-
