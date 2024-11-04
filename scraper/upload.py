9957a8349d74da8231b49fdce3f0fa26a64f7674
boroboro01 <duke7201@naver.com>
2024-11-04 20:13:43 +0900

    fix: fix file update to s3

--- scraper/upload.py
+++ scraper/upload.py
@@ -7,15 +7,17 @@ OBJECT_NAME_TO_UPLOAD = 'output.csv'
 s3_client = boto3.client(
     's3',
     aws_access_key_id = constants.access_key,
-    aws_secret_access_key = constants.secret_access_key
+    aws_secret_access_key = constants.secret_access_key,
+    region_name = 'us-east-1'
 )
 
 response = s3_client.generate_presigned_post(
     Bucket = 'dorm-menu',
     Key = OBJECT_NAME_TO_UPLOAD,
-    ExpiresIn = 10
+    ExpiresIn = 60
 )
 
 files = {'file' : open(OBJECT_NAME_TO_UPLOAD, 'rb')}
 r = requests.post(response['url'], data=response['fields'], files=files)
-print(r.status_code)
+print(f"Status Code: {r.status_code}")
+print(f"Response Text: {r.text}")
